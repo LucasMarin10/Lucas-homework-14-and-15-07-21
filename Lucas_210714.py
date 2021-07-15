a4902bdd69c9e412a43d5f298305b3386c896c5f
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-07-15 11:19:31 -0300

    Add files via upload

---
+++ Lucas_210714.py
@@ -0,0 +1,26 @@
+
+print("question 2")
+python={"string":"Lucas","numbers":10,"list":["camelion","cacau","amora"]}
+print("question 3")
+print(python)
+print("question 4")
+print(python["string"])
+print("question 5")
+for shusi in python.keys():
+    print(shusi)
+print("question 6")
+for saschi in python.values():
+    print(saschi)
+print("question 7")
+for pithon in python.items():
+    print(pithon)
+print("question 8")
+print(sorted(python.keys()))
+print("question 9")
+lisk=[]
+for sdm in range(10):
+    lisk.append(python)
+print(lisk)
+
+
+    
